0f74146add53e33e03f4f8fcf694adbd74cef1b4
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2025-02-20 18:28:45 +0100

    Init

---
+++ Config.py
@@ -0,0 +1,29 @@
+import yaml
+import os
+
+config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
+
+
+class Config:
+
+    def __init__(self, config_path):
+        self.config_path = config_path
+        with open(config_path, "r") as file:
+            self.config = yaml.load(file, Loader=yaml.FullLoader)
+
+    def __str__(self):
+        return str(self.config)
+
+    def load(self, config_path):
+        self.config_path = config_path
+        with open(config_path, "r") as file:
+            self.config = yaml.load(file, Loader=yaml.FullLoader)
+
+    def save(self):
+        with open(self.config_path, "w") as file:
+            yaml.dump(self.config, file)
+
+
+main_config = Config(config_path)
+
+__all__ = ["main_config"]
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hydrangea-Jupyrender
+
+A python made tool for rendering ipynb files in read-only mode.
